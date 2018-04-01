88c039caf01a23010dc34e1561a0e8aab876a606
turtledust <peterkbone@yahoo.co.uk>
2018-04-01 06:13:48 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Code was added in github
+            //Code was edited in github
         }
     }
 }
